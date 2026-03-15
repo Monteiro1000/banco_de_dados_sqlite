57bf31f2a7a0cb5465e49f7bd2e99d29ac03234b
Júlio Monteiro <julioalvedmonteiro@gmail.com>
2026-03-15 13:52:05 -0300

    Revisando assuntos iniciais de banco de dados

--- aula02/insert.sql
+++ aula02/insert.sql
@@ -1,6 +1,21 @@
 --database: ./createTable.sqlite
 
 CREATE TABLE "cursos"("id", "nome", "aula");
-SELECT * FROM cursos;
+INSERT INTO "cursos"
+    ("id", "nome", "aula")
+  VALUES
+    (4, 'Monteiro', 10),
+    (7, 'Rafa', 15),
+    (10, 'Lucas', 17);
+
+    SELECT * FROM "cursos"
+     WHERE "id" = 7
+      OR
+    "nome" = 'Lucas';
+
+SELECT * FROM "cursos";
+
+DROP TABLE "cursos";
+
 
 INSERT INTO cursos (nome, aula) VALUES ('CSS', 12);
