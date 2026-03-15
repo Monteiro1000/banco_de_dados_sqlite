546148b0fa9af6c4432cd9a4e11c9b1eb540bb58
Júlio Monteiro <julioalvedmonteiro@gmail.com>
2026-03-15 18:24:39 -0300

    Exercicio sobre primeiro módulo de sqlite concluído

---
+++ exerciciosqlite/ex.sql
@@ -0,0 +1,40 @@
+--database: ./createTable.sqlite
+
+CREATE TABLE "produtos"(
+    "id" INTEGER NOT NULL,
+     "nome" TEXT NOT NULL,
+      "preco" INTEGER NOT NULL
+      );
+INSERT INTO
+ "produtos"("id", "nome", "preco")
+VALUES
+(1, 'Notebook', 1000),
+(2, 'Smarthphone', 500),
+(3, 'Tablet', 300);
+
+CREATE TABLE "clientes"(
+    "id" INTEGER NOT NULL,
+     "nome" TEXT NOT NULL,
+      "email" TEXT NOT NULL
+      );
+INSERT INTO
+ "clientes" ("id", "nome", "email")
+VALUES
+(1, 'Maria', 'maria@email.com'),
+(2, 'João', 'joao@email.com');
+
+
+CREATE TABLE "compras"
+("id" INTEGER NOT NULL, "cliente_id" INTEGER NOT NULL, "produto_id" INTEGER NOT NULL, "data" TEXT NOT NULL);
+INSERT INTO "compras"
+("id", "cliente_id", "produto_id", "data")
+VALUES
+(1, 2, 1, '2049-01-01'),
+(2, 1, 2, '2049-01-02'),
+(3, 2, 3, '2049-01-03');
+
+SELECT * FROM produtos;
+
+SELECT "nome" FROM "produtos" WHERE "preco" > 400;
+
+SELECT * FROM "compras" WHERE "cliente_id" = '2';
